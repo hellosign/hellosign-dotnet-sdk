891fbc49dcaf6c6ecb61b50dd0b96806c2c06d28
Stephen Eisenhauer <stephen@hellosign.com>
2015-05-29 20:05:20 -0700

    Add some preliminary AssemblyInfo metadata

--- HelloSign/Properties/AssemblyInfo.cs
+++ HelloSign/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HelloSign")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Client library for using the HelloSign API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HelloSign")]
 [assembly: AssemblyProduct("HelloSign")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("HelloSign")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- HelloSignTestApp/Properties/AssemblyInfo.cs
+++ HelloSignTestApp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HelloSignTestApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An example program demonstrating the HelloSign .NET SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HelloSign")]
 [assembly: AssemblyProduct("HelloSignTestApp")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
