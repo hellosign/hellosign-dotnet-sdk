ef1289242ddebc3b23d859a4a0943c5c0d037cb9
Stephen Eisenhauer <stephen@hellosign.com>
2015-10-20 18:56:09 -0700

    Add nuspec and bump version to 0.1.0.0

--- HelloSign/Properties/AssemblyInfo.cs
+++ HelloSign/Properties/AssemblyInfo.cs
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+// [assembly: AssemblyVersion("0.0.0.0")]
+// NOTE: This gets reported in the user-agent string for HTTP requests made.
+[assembly: AssemblyFileVersion("0.1.0.0")]
