f30fe0a2d04e69a632715bef1851ffe705c1db51
Stephen Eisenhauer <stephen@hellosign.com>
2016-05-10 00:13:55 -0700

    Metadata tweaks in preparation for NuGet publishing

--- HelloSign/Properties/AssemblyInfo.cs
+++ HelloSign/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// [assembly: AssemblyVersion("1.0.*")]
-// [assembly: AssemblyVersion("0.0.0.0")]
 // NOTE: This gets reported in the user-agent string for HTTP requests made.
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+// [assembly: AssemblyVersion("0.0.0.0")]
+// [assembly: AssemblyFileVersion("0.0.0.0")]
