be5d5c7dc07317227902c8b8ae8f1332cdc46458
John Spaetzel <john.spaetzel@hellosign.com>
2018-01-25 11:49:56 -0800

    Add missing fields and a test for custom fields

--- HelloSign/Models/CustomField.cs
+++ HelloSign/Models/CustomField.cs
@@ -10,5 +10,18 @@ namespace HelloSign
         public string Name { get; set; }
         public string Value { get; set; }
         public string Type { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool Required { get; set; }
+        public string ApiId { get; set; }
+        public CustomFieldInfo AvgTextLength { get; set; }
+
+        public class CustomFieldInfo 
+        {
+            public int NumLines { get; set; }
+            public int NumCharsPerLine { get; set; }
+        }
     }
 }
