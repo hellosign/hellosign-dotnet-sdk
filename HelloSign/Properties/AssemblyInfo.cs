a6171ea3b779690f8680764e2d62eb2c676d098e
John Spaetzel <john.spaetzel@hellosign.com>
2018-11-20 18:28:05 -0800

    version bump

--- HelloSign/Properties/AssemblyInfo.cs
+++ HelloSign/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // NOTE: This gets reported in the user-agent string for HTTP requests made.
-[assembly: AssemblyVersion("0.5.7.0")]
+[assembly: AssemblyVersion("0.5.8.0")]
 // [assembly: AssemblyVersion("0.0.0.0")]
 // [assembly: AssemblyFileVersion("0.0.0.0")]
