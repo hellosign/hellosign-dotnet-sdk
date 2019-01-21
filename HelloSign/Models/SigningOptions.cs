ce7f3fa04c893755a5dd6ad735397237b1e344c8
Stephen Eisenhauer <stephen@hellosign.com>
2019-01-21 13:32:45 -0800

    Add forgotten file

---
+++ HelloSign/Models/SigningOptions.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace HelloSign
+{
+    /// <summary>
+    /// Signing Options for a Signature Request
+    /// </summary>
+    public class SigningOptions
+    {
+        [JsonProperty("draw")]
+        public bool Draw { get; set; } = false;
+        [JsonProperty("type")]
+        public bool Type { get; set; } = false;
+        [JsonProperty("upload")]
+        public bool Upload { get; set; } = false;
+        [JsonProperty("phone")]
+        public bool Phone { get; set; } = false;
+        [JsonProperty("default")]
+        public string Default { get; set; } = "draw";
+    }
+}
