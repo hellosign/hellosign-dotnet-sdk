8f3512360c92136d52cfda018ab3954dc425615b
Stephen Eisenhauer <stephen@hellosign.com>
2019-01-24 23:04:53 -0800

    Add missing deserialization fields to UnclaimedDraft (fixes #40)

--- HelloSign/Models/UnclaimedDraft.cs
+++ HelloSign/Models/UnclaimedDraft.cs
@@ -12,8 +12,9 @@
         
         public string ClaimUrl { get; set; }
         public string SigningRedirectUrl { get; set; }
+        public string RequestingRedirectUrl { get; set; }
         public int? ExpiresAt { get; set; }
         public bool? TestMode { get; set; }
-
+        public string SignatureRequestId { get; set; }
     }
 }
