bc26c9654231df6f19d05a41ea8f22f297414b9f
Collin Wheeler <collinwheeler@hellosign.com>
2022-05-05 09:56:57 -0400

    Removed commented code

--- src/HelloSign/Models/ApiApp.cs
+++ src/HelloSign/Models/ApiApp.cs
@@ -10,31 +10,10 @@ namespace HelloSign
     {
         public string ClientId { get; set; }
         public string Name { get; set; }
-        //private string Domain;
         public List<string> Domains { get; set; }
         public string CallbackUrl { get; set; }
         public bool? IsApproved { get; set; }
         public Account OwnerAccount { get; set; }
         public Oauth Oauth { get; set; }
-
-        /*public void setDomain(string domain)
-        {
-            this.Domain = domain;
-        }
-
-        public void setDomain(List<string> domains)
-        {
-            this.Domains = domains;
-        }
-
-        public string getDomain()
-        {
-            return this.Domain ?? this.Domains.First();
-        }
-
-        public List<string> getAllDomains()
-        {
-            return this.Domains;
-        }*/
     }
 }
